a46a7bd7d94f7a215fcd94280edafbc4c735e743
Nicolás Ríos <riosnicolas618@gmail.com>
2023-02-03 01:10:56 -0300

    ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+"Nicolás"
+22
+true
+new Date('March 02, 2000')
+let libro = {
+	title: 'El Conde de Montecristo',
+	autor: 'Alexandre Dumas',
+	fecha: 1844,
+	url: 'https://es.wikipedia.org/wiki/El_conde_de_Montecristo'
+}
